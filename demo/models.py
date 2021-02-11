f30a46f09622621e22554b42bd64af2bd9e8a97a
mehedi <mehedi.hasan.shuvon@gmail.com>
2021-02-11 20:06:01 +0600

    testing models

--- demo/models.py
+++ demo/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title=models.CharField(max_length=36)
-    
+    STATUSES =(
+        (0,'Unknown'),
+        (1,'processed'),
+        (2,'paid'),
+    )
+    title=models.CharField(max_length=36,blank=False,unique=True,default='',choices=STATUSES)
+    #blank=true means this field can not be blanked and unique=true is slef explainatory
+    #here choices takes a tuple and it meands  we can only pick the those choices only
